b86371c95c1f1a3dae03fada6d222552ee859a6e
deepali <pateriyadeepali1008@gmail.com>
2024-02-27 11:57:24 +0530

    commit done

---
+++ R_Practise_day4.R
@@ -0,0 +1,38 @@
+x = 4
+(x>2) && (x<5) 
+(x[1]>2) & (x[1]<5)
+
+seq(from=2,by=3,leng.out=100) -> number.series
+number.series
+number.series[c(5, 10 ,15,20,30,101)]
+
+number.series[seq(from=5,to=20,by=5)]
+
+number.series[10:30]
+
+c("purple","gold","orange","pink") -> mouse.colour
+mouse.colour[2]
+
+
+list(23,21,18,26) -> mouse.weight
+mouse.weight
+
+
+data.frame(colour=mouse.colour,weight=mouse.weight) -> mouse.info
+mouse.info
+
+c(23,21,18,26) -> mouse.weight
+mouse.weight
+
+
+data.frame(colour=mouse.colour,weight=mouse.weight) -> mouse.info
+mouse.info
+
+
+mouse.info[3,]
+
+mouse.info[(1)]
+
+mouse.info[,1]
+
+mouse.info[4,3]
